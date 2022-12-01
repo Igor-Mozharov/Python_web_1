42d87203c33c9e665ebbe2e34260168d8277fc8d
Igor-Mozharov <r5@ukr.net>
2022-12-01 13:22:39 +0200

    12

--- power9bot/classes/note_imaginator.py
+++ power9bot/classes/note_imaginator.py
@@ -3,4 +3,4 @@ from power9bot.classes.default_imaginator import DefaultImaginator
 
 class NoteImaginator(DefaultImaginator):
     def imagination(self):
-        print(self.value)
+        return self.value
